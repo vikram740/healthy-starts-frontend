061387aeae317774a7028c8ce7b24a8372c04369
vikram740 <vikram@wiznsoft.com>
2026-02-14 18:21:07 +0530

    Add login/register toggle auth form with matching layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/auth/login/login";
 
 function App() {
-  return (
-    <Routes>
-      {/* Default route */}
-      <Route path="/" element={<Navigate to="/login" />} />
-
-      {/* Login */}
-      <Route path="/login" element={<Login />} />
-
-      {/* Fallback */}
-      <Route path="*" element={<h2>Page Not Found</h2>} />
-    </Routes>
-  );
+  return <Login />;
 }
 
 export default App;
